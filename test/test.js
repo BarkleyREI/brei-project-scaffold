ffafb70bb8ef496b2ab3102928a4ef8c45a62e3d
Ian Moffitt <IMoffitt@barkleyrei.com>
2019-03-04 09:02:24 -0500

    Add tests and update package.json files with new versions and dependencies.

--- test/test.js
+++ test/test.js
@@ -1,79 +1,61 @@
-const assert = require('yeoman-assert');
-const fs = require('fs');
-const _ = require('lodash');
+/*global describe, it, require, __dirname*/
+
+'use strict';
+
+const util = require('brei-util');
 
 const root = __dirname + '/..';
 
-const ignored = [
-	'.git',
-	'node_modules',
-	'.DS_Store'
-];
-
-let rootFiles = [
-	'package.json',
-	'README.md',
+let valid = [
 	'.gitignore',
-	'src',
-	'test'
-];
-
-let configFiles = [
-	'.eslintrc.json',
-	'.stylelintrc.json',
-	'assemblefile.js',
-	'bs.config.js',
-	'modernizr-config.json',
-	'postcss.config.js',
-	'webpack.config.js'
+	'README.md',
+	{
+		_config:
+			['.eslintrc.json',
+				'.stylelintignore',
+				'assemblefile.js',
+				'bs.config.js',
+				'modernizr-config.json',
+				'postcss.config.js',
+				'webpack.config.js']
+	},
+	{
+		app: [
+			{ assemble: [] },
+			{
+				ejs: [
+					{
+						lib: [
+							'jquery.js'
+						]
+					},
+					'main.js'
+				]
+			},
+			{ img: [] },
+			{ scss: [] },
+		]
+	},
+	{
+		lib: ['updateScss.js']
+	},
+	'package.json',
+	{
+		test: ['test.js']
+	}
 ];
 
 describe('Verify file and folder structure', function () {
 
-	it('Root file check', function () {
+	it('Deep object comparison check', function () {
 
-		let files = filesArray(root);
+		let ttree = util.tree(root);
 
-		assert(has(rootFiles, files));
+		let files = util.ftree(ttree);
 
-	});
-
-	it('Config check', function () {
-
-		let files = filesArray(root + '/_config');
-
-		assert(hasOnly(configFiles, files));
+		util.assert(util.deep(valid, files));
 
 	});
 
 });
 
-function has(list, arr) {
-
-	let diff = _.difference(list, arr);
-
-	if (diff.length === 0) {
-		return true;
-	} else {
-		throw new Error("Missing: " + diff);
-	}
-
-}
-
-function hasOnly(list, arr) {
-
-	// Remove ignored files and directories
-	arr = _.difference(arr, ignored);
-
-	let forward = _.difference(list, arr);
-	let backward = _.difference(arr, list);
-
-	return forward.length === backward.length;
-
-}
-
-function filesArray(dir) {
-
-	return fs.readdirSync(dir);
-
-}

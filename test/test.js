02cee919512116f46e75d397b96e22c9ba9ee355
Ian Moffitt <IMoffitt@barkleyrei.com>
2019-03-25 06:48:30 -0400

    Add blank config so that tests on a fresh generated project pass.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@
 'use strict';
 
 const util = require('brei-util');
-// const u = require('util');
+const u = require('util');
 
 const root = __dirname + '/..';
 
@@ -13,6 +13,7 @@ let valid = [
 		_config:
 			['.eslintrc.json',
 				'.stylelintignore',
+				'_brei.json',
 				'assemblefile.js',
 				'bs.config.js',
 				'modernizr-config.json',
@@ -53,16 +54,16 @@ describe('Verify file and folder structure', function () {
 
 		let files = util.ftree(ttree);
 
-		// console.log('\n------- files --------\n');
-		// console.log(u.inspect(files, false, null));
-		//
-		// console.log('\n------- valid --------\n');
-		// console.log(u.inspect(valid, false, null));
-		//
-		// console.log('\n------- test --------\n');
-		// console.log(u.inspect(util.deepNotOnly(valid, files), false, null));
+		console.log('\n------- files --------\n');
+		console.log(u.inspect(files, false, null));
 
-		util.assert(util.deep(util.deepNotOnly(valid, files), {}));
+		console.log('\n------- valid --------\n');
+		console.log(u.inspect(valid, false, null));
+
+		console.log('\n------- test --------\n');
+		console.log(u.inspect(util.deepNotOnly(valid, files), false, null));
+
+		util.assert(util.deep(util.deepNotOnly(files, valid), {}));
 
 	});
 
